961e74b3282779573be07552874ccbdd089b00f0
Quốc Hùng <Quochung1st-dev@users.noreply.github.com>
2025-08-17 10:48:34 +0700

    s

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // Import Custom CSS
 import './index.css'
 import App from './App.tsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
